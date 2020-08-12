826c2c324c160ad9a1666a007b058f7f1eeaa67c
anurag <anuragsinha4345@gmail.com>
2020-08-12 17:53:51 +0530

    Revert "for production commit"
    This reverts commit d4fa625b0134f49df80ee770c47e8cde915a906a.

--- server/index.js
+++ server/index.js
@@ -11,13 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", rounte)
 
-//production 
-app.use(express.static("../client/build"))
-const path = require("path");
-
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-})
 
 app.listen(port, () => {
     console.log(`${port} start `);
