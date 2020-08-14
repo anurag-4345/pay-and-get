f3ce51241aa5a49a4d1c713fb2e69feb6451ccf8
anurag <anuragsinha4345@gmail.com>
2020-08-14 16:47:43 +0530

    setup heroku mongo URI
    mongo URI setup

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    dev_URI: "mongodb+srv://anurag:anurag@project-eb8b0.mongodb.net/Record?retryWrites=true&w=majority"
+}
--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    URL: "mongodb+srv://anurag:anurag@project-eb8b0.mongodb.net/Record?retryWrites=true&w=majority",
-    URI: "mongodb://localhost:27017/block"
+if(process.env.NODE_ENV == "prodcution"){
+    module.exports = require("./prod")
+}else{
+    module.exports = require("./dev")
 }
