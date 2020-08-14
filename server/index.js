92c6c023abceba4bd848bf8e910a655384690323
anurag <anuragsinha4345@gmail.com>
2020-08-14 17:02:18 +0530

    server file
    set

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ app.use("/", rounte)
 app.use(express.static("../client/build"))
 const path = require("path");
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 })
 
 app.listen(port, () => {
