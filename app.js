bdea2ca0c8c931b7c76ccef7c8027f44cc7433a4
anurag <anuragsinha4345@gmail.com>
2020-08-22 01:54:12 +0530

    inistal commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var createNewRouter = require('./routes/createNew');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
